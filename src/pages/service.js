c765e7b8179cf71ba88f236ca1d990a2016e0969
AnneKsatn <AnneKsatn@gmail.com>
2023-03-21 13:14:41 +0300

    remove server

--- src/pages/service.js
+++ src/pages/service.js
@@ -9,8 +9,214 @@ const onChange = (key) => {
     console.log(key);
 };
 
+const tags = [
+    {
+      "id": "1",
+      "section_title": "Комнаты",
+      "tags": [
+        {
+          "title": "Ванная",
+          "isSelected": false
+        },
+        {
+          "title": "Гостиная",
+          "isSelected": false
+        },
+        {
+          "title": "Детская",
+          "isSelected": false
+        },
+        {
+          "title": "Кабинет",
+          "isSelected": false
+        },
+        {
+          "title": "Офис",
+          "isSelected": false
+        },
+        {
+          "title": "Прихожая",
+          "isSelected": false
+        },
+        {
+          "title": "Спальня",
+          "isSelected": false
+        },
+        {
+          "title": "Холл",
+          "isSelected": false
+        },
+        {
+          "title": "Коридор",
+          "isSelected": false
+        },
+        {
+          "title": "Уборная",
+          "isSelected": false
+        },
+        {
+          "title": "Туалет",
+          "isSelected": false
+        },
+        {
+          "title": "Гардероб",
+          "isSelected": false
+        },
+        {
+          "title": "Библиотека",
+          "isSelected": false
+        },
+        {
+          "title": "Домашний кинотеатр",
+          "isSelected": false
+        },
+        {
+          "title": "Тренажерный зал",
+          "isSelected": false
+        },
+        {
+          "title": "Зимний сад",
+          "isSelected": false
+        },
+        {
+          "title": "Сауна",
+          "isSelected": false
+        }
+      ]
+    },
+    {
+      "id": "2",
+      "section_title": "Стили",
+      "tags": [
+        {
+          "title": "лофт",
+          "isSelected": false
+        },
+        {
+          "title": "модерн",
+          "isSelected": false
+        },
+        {
+          "title": "классика",
+          "isSelected": true
+        },
+        {
+          "title": "арт-деко",
+          "isSelected": false
+        },
+        {
+          "title": "хай-тек",
+          "isSelected": false
+        },
+        {
+          "title": "этно",
+          "isSelected": false
+        },
+        {
+          "title": "неоклассика",
+          "isSelected": true
+        },
+        {
+          "title": "прованс",
+          "isSelected": true
+        },
+        {
+          "title": "скандинавский",
+          "isSelected": false
+        },
+        {
+          "title": "эклектика",
+          "isSelected": false
+        },
+        {
+          "title": "экостиль",
+          "isSelected": false
+        },
+        {
+          "title": "современный",
+          "isSelected": false
+        },
+        {
+          "title": "американский",
+          "isSelected": false
+        },
+        {
+          "title": "английский",
+          "isSelected": false
+        },
+        {
+          "title": "арабский",
+          "isSelected": false
+        },
+        {
+          "title": "японский",
+          "isSelected": false
+        },
+        {
+          "title": "француский",
+          "isSelected": false
+        }
+      ]
+    },
+    {
+      "id": "3",
+      "section_title": "Художники",
+      "tags": [
+        {
+          "title": "Леонардо да Винчи",
+          "isSelected": false
+        },
+        {
+          "title": "Пабло Пикассо",
+          "isSelected": false
+        },
+        {
+          "title": "Сальвадор Дали",
+          "isSelected": false
+        },
+        {
+          "title": "Клод Моне",
+          "isSelected": false
+        },
+        {
+          "title": "Сандро Боттичелли",
+          "isSelected": false
+        },
+        {
+          "title": "Микеланджело Буонарроти",
+          "isSelected": false
+        },
+        {
+          "title": "Фрида Кало",
+          "isSelected": false
+        }
+      ]
+    },
+    {
+      "id": "4",
+      "section_title": "Прочее",
+      "tags": [
+        {
+          "title": "уютная атмосфера",
+          "isSelected": false
+        },
+        {
+          "title": "современный дизайн",
+          "isSelected": false
+        },
+        {
+          "title": "высокое качество",
+          "isSelected": false
+        },
+        {
+          "title": "цветочные мотивы",
+          "isSelected": false
+        }
+      ]
+    }
+  ]
 
-export default function Home({ tags }) {
+export default function Home() {
     return (
         <div className={styles.layout}>
             <ServiceHeader />
@@ -44,12 +250,12 @@ export default function Home({ tags }) {
     );
 }
 
-export async function getServerSideProps() {
+// export async function getServerSideProps() {
 
-    const res = await fetch('/api')
-    const tags = await res.json()
+//     const res = await fetch('/api')
+//     const tags = await res.json()
 
-    return {
-        props: { tags: tags }
-    }
-}
+//     return {
+//         props: { tags: tags }
+//     }
+// }
